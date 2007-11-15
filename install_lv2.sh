43035a23da09da131bdad8af35c2f66cd837b721
nedko <nedko@7441ed9f-d024-0410-a868-8e1bb56d35c0>
2007-11-15 23:14:48 +0000

    Fix lv2 plugin install script to display helpful message for first time LV2 users (no LV2_PATH set)
    git-svn-id: svn://svn.gna.org/svn/zyn/code@162 7441ed9f-d024-0410-a868-8e1bb56d35c0

--- install_lv2.sh
+++ install_lv2.sh
@@ -3,25 +3,30 @@
 # Install lv2 plugin bundle
 #
 
-if test -z "${LV2_INSTALL_PATH}" -a "${LV2_PATH}"
+if test -z "${LV2_INSTALL_PATH}"
 then
-  echo "Trying to deduce install location from LV2_PATH contents..."
-  OLDIFS=${IFS}
-  IFS=:
-  for DEST in ${LV2_PATH}
-  do
-    if test -d "${DEST}" -a -w "${DEST}"
-    then
-      echo "${DEST} looks good."
-      LV2_INSTALL_PATH=${DEST}
-      break
-    else
-      echo "${DEST} is not writable directory..."
-    fi
-  done
-  #echo "${LV2_PATH} is directory. Installing there..."
-  #LV2_INSTALL_PATH=${LV2_PATH}
-  IFS=${OLDIFS}
+  if test "${LV2_PATH}"
+  then
+    echo "Trying to deduce install location from LV2_PATH contents..."
+    OLDIFS=${IFS}
+    IFS=:
+    for DEST in ${LV2_PATH}
+    do
+      if test -d "${DEST}" -a -w "${DEST}"
+      then
+        echo "${DEST} looks good."
+        LV2_INSTALL_PATH=${DEST}
+        break
+      else
+        echo "${DEST} is not writable directory..."
+      fi
+    done
+    #echo "${LV2_PATH} is directory. Installing there..."
+    #LV2_INSTALL_PATH=${LV2_PATH}
+    IFS=${OLDIFS}
+  else
+    echo "LV2_PATH is not set, not trying to auto-deduce install location"
+  fi
 fi
 
 if test -z "${LV2_INSTALL_PATH}"
